0f78e65df375bc8b2dbe92c20e52568266bc8f28
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-09-05 17:17:03 +0300

    added auth module

--- apps/full-stack/src/app/app.module.ts
+++ apps/full-stack/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AuthModule } from '@auth0/auth0-angular';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AuthModule.forRoot({
+      domain: 'dev-vftj-8hu.us.auth0.com',
+      clientId: 'kD6iPHFeWwxbjAUUxwY1U2vzS19kYDyd'
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })

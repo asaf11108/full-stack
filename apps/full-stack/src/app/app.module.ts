ff5c7f9615ad0442209c69d8cc4bece6d7fadf1e
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-09-05 22:47:22 +0300

    added material

--- apps/full-stack/src/app/app.module.ts
+++ apps/full-stack/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthModule } from '@auth0/auth0-angular';
 import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,6 +19,7 @@ import { environment } from '../environments/environment';
       clientId: 'kD6iPHFeWwxbjAUUxwY1U2vzS19kYDyd',
       redirectUri: environment.redirectUri
     }),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthComponent } from './auth/auth.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    MatButtonModule
+  ],
   declarations: [
     AuthComponent
   ],

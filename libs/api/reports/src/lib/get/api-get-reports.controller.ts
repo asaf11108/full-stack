70f36d3135f6255c1715216ed8adb9f02ad8780e
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-09-07 22:34:42 +0300

    req

--- libs/api/reports/src/lib/get/api-get-reports.controller.ts
+++ libs/api/reports/src/lib/get/api-get-reports.controller.ts
@@ -1,12 +1,39 @@
-import { Controller, Get, StreamableFile } from '@nestjs/common';
-import { createReadStream } from 'fs';
+import { map } from 'rxjs/operators';
+import { Website } from '@full-stack/interfaces';
+import { Controller, Get, Param } from '@nestjs/common';
 import { join } from 'path';
+import { from, Observable } from 'rxjs';
+import * as csv from 'csvtojson';
 
 @Controller(['reports/get'])
 export class ApiGetReportsController {
+    static headers = [
+        'websiteId',
+        'date',
+        'widgetId',
+        'clicks',
+        'impressions',
+        'revenue',
+    ];
+    static filePath = join(__dirname, 'assets/all_data.csv');
+
     @Get('allData')
-    allData(): StreamableFile {
-        const file = createReadStream(join(__dirname, 'assets/all_data.csv'));
-        return new StreamableFile(file);
-  }
+    allData(): Observable<Website[]> {
+        return this.readFile();
+    }
+
+    @Get()
+    Data(@Param() date: number): Observable<Website[]> {
+        return this.readFile().pipe(
+            map(allData => allData.filter(data => new Date(data.date).getTime() === date))
+        );
+    }
+
+    private readFile(): Observable<Website[]> {
+        return from(
+            csv({
+                headers: ApiGetReportsController.headers,
+            }).fromFile(ApiGetReportsController.filePath)
+        );
+    }
 }
--- libs/fe/main/src/lib/main/main.component.ts
+++ libs/fe/main/src/lib/main/main.component.ts
@@ -1,10 +1,6 @@
-import { Website } from '@full-stack/interfaces';
 import { environment } from './../../../../../../apps/fe/src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { switchMap } from 'rxjs/operators';
-import { from, Observable } from 'rxjs';
-import * as csv from 'csvtojson';
 
 @Component({
   selector: 'fe-main',
@@ -17,9 +13,7 @@ export class MainComponent implements OnInit {
 
   ngOnInit(): void {
     this.http
-      .get(environment.apiUrl + 'reports/get/allData', {responseType: 'text'}).pipe(
-        switchMap<string, Observable<Website[]>>(res => from(csv({ headers: ['websiteId', 'date', 'widgetId', 'clicks', 'impressions', 'revenue'] }).fromString(res)))
-      )
+      .get(environment.apiUrl + 'reports/get/allData')
       .subscribe(console.log);
   }
 }

bcef6f8f4b8344bc22b585e742c2235da4151e14
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-09-07 12:30:47 +0300

    added table

--- libs/fe/ui/src/lib/fe-ui.module.ts
+++ libs/fe/ui/src/lib/fe-ui.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [
+    TableComponent
+  ],
 })
 export class FeUiModule {}

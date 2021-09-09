b22564ac2602736a147012a5dea6b2bd8b6706c9
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-09-09 20:50:07 +0300

    table

--- libs/fe/ui/src/lib/components/table/table.component.ts
+++ libs/fe/ui/src/lib/components/table/table.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input, ViewEncapsulation } from '@angular/core';
-import { ColumnMode, TableColumn } from '@swimlane/ngx-datatable';
-import { BreakpointObserver } from '@angular/cdk/layout';
+import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Component, OnInit, ChangeDetectionStrategy, Input, ViewEncapsulation, ChangeDetectorRef, ViewChild } from '@angular/core';
+import { ColumnMode, DatatableComponent, TableColumn } from '@swimlane/ngx-datatable';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'fe-table',
@@ -14,15 +16,21 @@ import { BreakpointObserver } from '@angular/cdk/layout';
 })
 export class TableComponent<T> implements OnInit {
   ColumnMode = ColumnMode;
-  view: [number, number] = [1250, 700];
+  
+  view: [number, number];
 
   @Input() columns: TableColumn[];
   @Input() data: T[];
 
-  constructor(private breakpointObserver: BreakpointObserver) { }
+  constructor(private breakpointObserver: BreakpointObserver, private cdr: ChangeDetectorRef) { }
 
   ngOnInit(): void {
-    this.breakpointObserver.observe(['XSmall']).subscribe(console.log)
+    const COLUMN_WIDTH = 200;
+    const WIDTH_OFFSET = 50;
+    this.breakpointObserver.observe(Breakpoints.XSmall).subscribe(obs => {
+      this.view = (obs.matches ? [COLUMN_WIDTH + WIDTH_OFFSET, 500] : [COLUMN_WIDTH * this.columns.length + WIDTH_OFFSET, 700]);
+      this.cdr.markForCheck();
+    })
   }
 
 }

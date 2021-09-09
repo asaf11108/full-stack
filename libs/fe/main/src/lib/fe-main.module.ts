a3cb4b98a8908781a575603d0b9e3319750fc836
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-09-09 21:05:47 +0300

    main

--- libs/fe/main/src/lib/fe-main.module.ts
+++ libs/fe/main/src/lib/fe-main.module.ts
@@ -3,15 +3,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
 import { FeMainRoutingModule } from './fe-main-routing.module';
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
-import {NgPipesModule} from 'ngx-pipes';
+import { NgPipesModule } from 'ngx-pipes';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
-  imports: [CommonModule, FeMainRoutingModule, FeUiModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, NgPipesModule],
-  declarations: [
-    MainComponent
+  imports: [
+    CommonModule,
+    FeMainRoutingModule,
+    FeUiModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    NgPipesModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
+  declarations: [MainComponent],
 })
 export class FeMainModule {}

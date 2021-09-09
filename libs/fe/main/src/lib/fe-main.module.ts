cf9de9901ec56e7b3e78ec0efb2428434af3f285
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-09-09 20:50:18 +0300

    main

--- libs/fe/main/src/lib/fe-main.module.ts
+++ libs/fe/main/src/lib/fe-main.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
 import { FeMainRoutingModule } from './fe-main-routing.module';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, FeMainRoutingModule, FeUiModule],
+  imports: [CommonModule, FeMainRoutingModule, FeUiModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
   declarations: [
     MainComponent
   ],
--- libs/fe/main/src/lib/main/main.component.ts
+++ libs/fe/main/src/lib/main/main.component.ts
@@ -3,8 +3,8 @@ import { environment } from '@full-stack/fe/environment';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { TableColumn } from '@swimlane/ngx-datatable';
-import { Observable } from 'rxjs';
 import { Website } from '@full-stack/interfaces';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'fe-main',
@@ -13,13 +13,22 @@ import { Website } from '@full-stack/interfaces';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainComponent implements OnInit {
+  form = new FormGroup({
+    query: new FormControl()
+  });
+  controls = {
+    query: this.form.get('query') as FormControl
+  };
+
   constructor(private http: HttpClient) {}
 
   columns: TableColumn[] = TABLE_CONFIG;
-  websites$: Observable<Website[]>;
+  websites: Website[];
 
   ngOnInit(): void {
-    this.websites$ = this.http
-      .get<Website[]>(environment.apiUrl + 'reports/get/allData');
+    this.http.get<Website[]>(environment.apiUrl + 'reports/get/allData').subscribe(res => {
+      this.websites = res;
+    });
+
   }
 }

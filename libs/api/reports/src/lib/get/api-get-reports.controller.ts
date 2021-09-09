1a8fbf173c616111371ba18eba08f57b3cf6bc2f
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-09-09 22:00:54 +0300

    guard

--- libs/api/reports/src/lib/get/api-get-reports.controller.ts
+++ libs/api/reports/src/lib/get/api-get-reports.controller.ts
@@ -3,20 +3,17 @@ import { map, tap } from 'rxjs/operators';
 import { Website } from '@full-stack/interfaces';
 import { Controller, Get, Param, ParseIntPipe, UseGuards } from '@nestjs/common';
 import { Observable } from 'rxjs';
-import { AuthGuard } from '@nestjs/passport';
 
 @Controller(['reports/get'])
 export class ApiGetReportsController {
 
     constructor(private apiReportsService: ApiReportsService) {}
 
-    @UseGuards(AuthGuard('jwt'))
     @Get('allData')
     allData(): Observable<Website[]> {
         return this.apiReportsService.websites;
     }
 
-    @UseGuards(AuthGuard('jwt'))
     @Get(':time')
     data(@Param('time', ParseIntPipe) time: number): Observable<Website[]> {
         return this.apiReportsService.websites.pipe(

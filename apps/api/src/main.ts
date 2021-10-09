028fc53b12dc75d903e7b94802121051866d8bc9
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-10-09 12:30:28 +0300

    swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,6 +6,7 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as compression from 'compression';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
@@ -16,9 +17,22 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   app.use(compression());
   const port = process.env.PORT || 3333;
+
+  const config = new DocumentBuilder()
+    .setTitle('Websites example')
+    .setDescription('The websites API description')
+    .setVersion('1.0')
+    .addTag('websites')
+    .addBearerAuth(
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config, {  });
+  SwaggerModule.setup('api', app, document );
+
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
   });
+
 }
 
 bootstrap();
